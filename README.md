55d4b687db08b0217fdccb1d5e1a82c1e45f311d
useryesa9@gmail.com <useryesa9@gmail.com>
2025-03-12 13:45:06 +0700

    membuat table

--- README.md
+++
@@ -1,5 +0,0 @@
-Install GORM dan PostgreSQL driver
-```bash
-go get -u gorm.io/gorm
-go get -u gorm.io/driver/postgres
-```
--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"log"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+type Siswa struct {
+	ID       uint `gorm:"primaryKey"`
+	Nama     string
+	Email    string `gorm:"unique"`
+	Password string
+}
 
 func main() {
 	dsn := "host=localhost user=postgres password=root dbname=sekolah port=5432 sslmode=disable"
@@ -13,6 +21,12 @@ func main() {
 	if err != nil {
 		log.Fatal("Gagal konek ke database:", err)
 	}
-	log.Println("Berhasil konek ke database", db)
+	log.Println("Berhasil konek ke database")
+
+	err = db.AutoMigrate(&Siswa{})
+	if err != nil {
+		log.Fatal("Gagal migrate tabel:", err)
+	}
+	log.Println("Tabel users berhasil dibuat!")
 
 }
