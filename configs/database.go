0e8f86e1f6c0db052d0543f88dd6afb1129e77fd
useryesa9@gmail.com <useryesa9@gmail.com>
2025-03-13 20:40:06 +0700

    terhubung ke database

---
+++ configs/database.go
@@ -0,0 +1,20 @@
+package configs
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=root dbname=belajar_gorm port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Gagal terhubung ke database:", err)
+	}
+	DB = db
+	return DB
+}
--- main.go
+++ main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
-type Siswa struct {
-	ID       uint `gorm:"primaryKey"`
-	Nama     string
-	Email    string `gorm:"unique"`
-	Password string
-}
+import "belajar_gorm/configs"
 
 func main() {
-	dsn := "host=localhost user=postgres password=root dbname=sekolah port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		log.Fatal("Gagal konek ke database:", err)
-	}
-	log.Println("Berhasil konek ke database")
-
-	err = db.AutoMigrate(&Siswa{})
-	if err != nil {
-		log.Fatal("Gagal migrate tabel:", err)
-	}
-	log.Println("Tabel users berhasil dibuat!")
-
+	configs.ConnectDB()
 }
