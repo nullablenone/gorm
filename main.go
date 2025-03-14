16f857c69cdfa6a26626f49905e4249635439868
useryesa9@gmail.com <useryesa9@gmail.com>
2025-03-14 13:37:57 +0700

    menambil 1 data

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"belajar_gorm/configs"
 	"belajar_gorm/models"
+	"fmt"
 	"log"
 )
 
 func main() {
 	configs.ConnectDB()
 	err := configs.DB.AutoMigrate(&models.User{})
-	if err != nil{
+	if err != nil {
 		log.Fatal("Gagal membuat table:", err)
 	}
 
-	yesa := models.User{Nama: "muhamad yesa", Email: "yesa@gmail.com"}
-	configs.DB.Create(&yesa)
+	// menambah data
+	// yesa := models.User{Nama: "muhamad yesa", Email: "yesa@gmail.com"}
+	// configs.DB.Create(&yesa)
+
+	// mengambil data
+	var user models.User
+	configs.DB.First(&user, 1)
+	fmt.Printf("ID : %d , Nama : %s. Email : %s ", user.ID, user.Nama, user.Email)
 }
